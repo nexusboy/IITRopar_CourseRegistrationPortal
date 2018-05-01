66ac02196e53e6827f9209f584d835b712822bc8
nexusboy <nexusboy@github.com>
2018-05-01 17:53:46 +0530

    init_repository

---
+++ server.php
@@ -0,0 +1,91 @@
+<?php
+session_start();
+
+$username = "";
+$password = "";
+$errors = array();
+// connect to postgresql
+
+$db_connection = mysqli_connect('localhost', 'root', '', 'university_database');
+// Check The Connection
+if (!$db_connection)
+{
+    die("Connection Failed: " . mysqli_connect_error());
+}
+// Login user
+mysqli_select_db($db_connection,"university_database");
+if(isset($_POST['login_user'])){
+   /* echo "entered here <br>";
+    $username = mysqli_real_escape_string($db_connection,$_POST['username']) ;
+    $password = mysqli_real_escape_string($db_connection,$_POST['password']) ;
+   if (empty($username)) {
+        array_push($errors, "Username is required");
+    }
+    if (empty($password)) {
+        array_push($errors, "Password is required");
+    }*/
+
+
+    /*
+    $query = "SELECT * FROM university_model.user_list WHERE username=$username AND password='$password'";
+    $results = mysqli_query($db_connection, $query);
+    $rows = array();
+    while($r = mysqli_fetch_assoc($results)) {
+        $rows[] = $r;
+    }
+    echo json_encode($rows) . "<br>";
+    echo mysqli_num_rows($results) ;
+    echo  "<br>";
+
+    if (empty($username)) {
+        array_push($errors, "Username is required");
+    }
+    if (empty($password)) {
+        array_push($errors, "Password is required");
+    }
+
+
+   if (mysqli_num_rows($results) == 1) {
+         echo "entered here ";
+       $_SESSION['username'] = $username;
+       $_SESSION['success'] = "You are now logged in";
+        header('location: studentloggedIn.php');
+        echo "login succesful";
+    }
+    */
+    $username = mysqli_real_escape_string($db_connection,$_POST['username']) ;
+    $password =mysqli_real_escape_string($db_connection,$_POST['password']) ;
+    if (empty($username)) {
+        array_push($errors, "Username is required");
+    }
+    if (empty($password)) {
+        array_push($errors, "Password is required");
+    }
+
+    if (count($errors) == 0 ) {
+
+        $query = "SELECT * FROM university_model.user_list WHERE username=$username AND password='$password'";
+        $results = mysqli_query($db_connection, $query);
+        if (mysqli_num_rows($results) == 1 ) {
+            $r = mysqli_fetch_assoc($results);
+            if(strcmp($r['type'],"student") == 0 ) {
+                $_SESSION['username'] = $username;
+                $_SESSION['success'] = "You are now logged in";
+                header('location: studentloggedIn.php');
+                echo "login succesful";
+            }
+            if(strcmp($r['type'],"teacher") == 0 ) {
+                $_SESSION['username'] = $username;
+                $_SESSION['success'] = "You are now logged in";
+                header('location: teacherLoggedIn.php');
+                echo "login succesful";
+            }
+        }
+        else {
+            array_push($errors, "Wrong username/password combination");
+        }
+    }
+
+}
+
+?>
---
+++ teacherLoggedIn.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Don
+ * Date: 5/1/2018
+ * Time: 3:06 PM
+ */
+session_start();
+if (isset($_GET['logout'])) {
+    echo "entered here";
+    session_destroy();
+    unset($_SESSION['username']);
+    header("location: login.php");
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome Student <?php
+        echo $_SESSION['username'];
+        ?></title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<h1> Hello teacher </h1>
+<body>
+<?php if (isset($_SESSION['username'])) : ?>
+    <!--    -->
+    <p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
+    <p> <a href="teacherLoggedIn.php?logout='1'" style="color: red;">logout</a> </p>
+<?php endif ?>
+
+</body>>
+</html>
