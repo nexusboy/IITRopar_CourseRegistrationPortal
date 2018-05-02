e6d6448307ef410b7bbb81256c47103ce753da27
nexusboy <nexusboy@github.com>
2018-05-03 05:06:26 +0530

    added inserting the courses

--- studentTranscript.php
+++ studentTranscript.php
@@ -16,7 +16,7 @@ if (!$db_connection) {
 mysqli_select_db($db_connection, "crp");
 $sql1 = "SELECT id,courseid,grade
 FROM grades
-WHERE id=$student_id/*Given by the Staff*/ and semester =7;"
+WHERE id=$student_id/*Given by the Staff*/ and semester =7;";
 $result = mysqli_query($db_connection, $sql1);
 $no = 1;
 $credits = 0;
@@ -33,4 +33,24 @@ while ($row = mysqli_fetch_row($result)) {
     $credits = $credits + $row[1];
 }
 
+function calc_gpa($db_conncection,$s_id){
+    $query = "SELECT sum(grade*grades.credits)/SUM(grades.credits) FROM grades INNER JOIN courses ON grades.courseid = courses.id
+WHERE grades.id=$s_id;";
+    $query_run =  mysqli_query($db_conncection,$query);
+    if(!$query_run)
+    {
+        echo "Error: " . mysqli_error($db_conncection);;
+        exit();
+    }
+
+    while ($row = mysqli_fetch_row($query_run)) {
+    }
+    return $row[0];
+}
+
+
+
+$student_id = 1 ;
+$gpa = calc_gpa($db_connection,$student_id);
+echo "GPA of the student".$gpa;
 ?>
